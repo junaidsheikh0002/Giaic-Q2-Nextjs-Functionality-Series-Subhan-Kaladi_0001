6e11d08334783d307f845f3a4859ec3d3229bb15
Junaid Ul Haque sheikh <156045075+junaidsheikh0002@users.noreply.github.com>
2025-01-08 00:51:00 +0500

    https://github.com/junaidsheikh0002/Giaic-Q2-Nextjs-Functionality-Series-Subhan-Kaladi_0001/upload/main
    https://github.com/junaidsheikh0002/Giaic-Q2-Nextjs-Functionality-Series-Subhan-Kaladi_0001/upload/main

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
     domains: ['images.unsplash.com', 'via.placeholder.com'],
+    unoptimized: true
   },
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  output: 'standalone'
 }
 
 module.exports = nextConfig
